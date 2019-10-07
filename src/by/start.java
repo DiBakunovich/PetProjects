6cbc97831bc8d5301fac94cf957ba354315ee648
Dima <dima-5562@outlook.com>
2019-10-07 10:01:33 +0300

    Правка start

--- src/by/start.java
+++ src/by/start.java
@@ -4,6 +4,6 @@ public class Start {
 	public static void main(String[] args) throws Exception {
 		System.out.println("hello world вапва dhfgdhjsgdhfj");
 	}
-	mnk;
+	System.out.println("!!!");
 	nnkjn;
 }

5bdd5ee8b8dada8b6b3bbcb2e476e4b0d304bb9c
DBakunovich <dima-5562@outlook.com>
2019-10-08 16:57:55 +0300

    Доработка start1

--- src/by/start.java
+++ src/by/start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) throws Exception {
 		System.out.println("hello world вапва dhfgdhjsgdhfj");
 	}
+	sdfdsf
 	System.out.println("!!!");
 	nnkjn;
+	dsfdsf
 }

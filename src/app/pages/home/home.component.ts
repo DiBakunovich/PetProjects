9eaa4e9df67c465ed2703bea5169a79dd2a25f97
Bakunovich <dima-5562@outlook.com>
2023-04-24 15:29:05 +0300

    Описание к фото

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,78 +21,116 @@ export class HomeComponent implements OnInit {
     this.slides[0] = {
       id: 0,
       src: './assets/photo/home12_0.webp',
+      title: 'Домик',
+      subtitle: 'Внешний вид домика',
     };
     this.slides[1] = {
       id: 1,
       src: './assets/photo/home12_1.jpg',
+      title: 'Домик',
+      subtitle: 'Вид со стороны веранды',
     };
     this.slides[2] = {
       id: 2,
       src: './assets/photo/home12_2.jpg',
+      title: 'Домик',
+      subtitle: 'Вид из домика',
     };
     this.slides[3] = {
       id: 3,
       src: './assets/photo/home12_3.jpg',
+      title: 'Домик',
+      subtitle: 'Зона "кухни"',
     };
     this.slides[4] = {
       id: 4,
       src: './assets/photo/vip.webp',
+      title: 'Вип-домик',
+      subtitle: 'Внешний вид домика',
     };
     this.slides[5] = {
       id: 5,
       src: './assets/photo/vip1_1.jpg',
+      title: 'Вип-домик',
+      subtitle: 'Вид со стороны веранды',
     };
     this.slides[6] = {
       id: 6,
       src: './assets/photo/vip1_2.jpg',
+      title: 'Вип-домик',
+      subtitle: 'Вид из домика',
     };
     this.slides[7] = {
       id: 7,
       src: './assets/photo/vip1_3.jpg',
+      title: 'Вип-домик',
+      subtitle: 'Спальня',
     };
     this.slides[8] = {
       id: 8,
       src: './assets/photo/vip1_4.jpg',
+      title: 'Вип-домик',
+      subtitle: 'Уборная',
     };
     this.slides[9] = {
       id: 9,
       src: './assets/photo/vip1_5.jpg',
+      title: 'Вип-домик',
+      subtitle: 'Веранда на 2-ом этаже',
     };
     this.slides[10] = {
       id: 10,
       src: './assets/photo/pt1.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[11] = {
       id: 11,
       src: './assets/photo/pt2.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[12] = {
       id: 12,
       src: './assets/photo/pt3.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[13] = {
       id: 13,
       src: './assets/photo/pt4.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[14] = {
       id: 14,
       src: './assets/photo/pt5.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[15] = {
       id: 15,
       src: './assets/photo/pt6.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[16] = {
       id: 16,
       src: './assets/photo/pt7.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[17] = {
       id: 17,
       src: './assets/photo/pt8.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
     this.slides[18] = {
       id: 18,
       src: './assets/photo/pt9.jpg',
+      title: 'Фото от посетителей',
+      subtitle: '',
     };
   }
 
